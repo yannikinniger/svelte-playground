62d7bb481c2d68d5371fd081a76ba89a281f8055
Yannik Inniger <yannik.inniger@gmail.com>
2019-11-21 11:43:09 +0100

    Add select component

---
+++ src/inputs/Select.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let value;
+  export let options = [];
+  export let label = "NOT SET";
+</script>
+
+<select bind:value>
+  {#each options as option}
+    <option selected>Please select</option>
+    <option value={option.value}>{option.label}</option>
+  {/each}
+</select>
