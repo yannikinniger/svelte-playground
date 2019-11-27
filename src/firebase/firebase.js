969a178d92e7a783ada4d33fb15117b5f7433955
Yannik <yannik.inniger@gmail.com>
2019-11-27 14:49:37 +0100

    Add firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase/app';
+import 'firebase/auth'
+import 'firebase/firestore'
+import firebaseConfig from './firebaseConfig';
+
+
+firebase.initializeApp(firebaseConfig)
+
+export const auth = firebase.auth()
+export const db = firebase.firestore()
